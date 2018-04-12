c89b03bef77308106e16897185e295abd7dde859
Administrator <becwal@yandex.ru>
2018-04-12 19:43:27 +0300

    дизайн главной страницы

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => 'Лендинг CMS',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -69,7 +69,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Valerij <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
--- views/site/index.php
+++ views/site/index.php
@@ -7,47 +7,134 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Сайт на Yii2 за 5 дней</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Хотите получить красивый сайт за неделю?</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Тогда идите к нам!</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Для кого:</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>
+                    <ul>
+                        <li> для новичков в программировании на php и yii2</li>
+                        <li> для тех, кто хочет быстро создать современный сайт</li>
+                        <li> для тех, кто ценит свое время</li>
+                    </ul>
+                </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Описание:</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>
+                    На курсе мы изучим фреймворк Yii2
+                    и научимся создавать современные красивые
+                    адаптивные покупаемые сайты
+                </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Что изучим</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p> теорию и практику работы с#
+                    Php, Yii2,
+                    HTML5, CSS3, bootstrap,
+                    JS,jQuery
+                </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+
             </div>
         </div>
 
+    </div>
+    <div class="jumbotron">
+        <p><a class="btn btn-success" href="http://www.yiiframework.com/doc/">Подписаться&raquo;</a></p>
+    </div>
+    <div class="body-content">
+        <h1>Отзывы</h1>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Иванов Иван</h2>
+
+                <p>
+                    Прекрасный курс, отлично всё рассказывает автор,
+                    тему изучил быстро и легко,
+                    смог создать свой блог за 3 дня
+                </p>
+
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Петр Петров</h2>
+
+                <p> Отличный курс, большое подспорье фрилансеру
+                </p>
+
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Ирина Сидорова</h2>
+
+                <p>Прекрасный курс, быстро проходишь, много знаний остается</p>
+
+
+            </div>
+        </div>
+        <div class="jumbotron">
+            <p><a class="btn btn-success" href="http://www.yiiframework.com/doc/">Подписаться&raquo;</a></p>
+        </div>
+
+    </div>
+    <div class="body-content">
+        <h1>Выберите нужный тариф</h1>
+        <table class="table table-hover">
+            <tr>
+                <th>
+                    Условия
+                </th>
+                <th>
+                    Эконом
+                </th>
+                <th>
+                    Бизнес
+                </th>
+                <th>
+                    Эксперт
+                </th>
+            </tr>
+            <tr>
+                <td>Кол-во дней</td>
+                <td>5</td>
+                <td>10</td>
+                <td>15</td>
+            </tr>
+            <tr>
+                <td>Консультации автора</td>
+                <td>-</td>
+                <td>+</td>
+                <td>+</td>
+            </tr>
+            <tr>
+                <td>Бонус-курс</td>
+                <td>-</td>
+                <td>-</td>
+                <td>+</td>
+            </tr>
+            <tr>
+                <th>Цена</th>
+                <th>500</th>
+                <th>1000</th>
+                <th>3000</th>
+            </tr>
+        </table>
     </div>
 </div>
